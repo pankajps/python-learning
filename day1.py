a4f2b5fa0e1fc3ae56ac49bc6541bb47e8e2a470
Pankaj Pratap SIngh <pankajsys.engg@gmail.com>
2025-04-25 16:44:00 +0400

    exercise commit

--- day1.py
+++ day1.py
@@ -1,36 +1,36 @@
-#Write a Python program to print "Hello, World!".
+# Write a Python program to print "Hello, World!".
 print("Hello, World!")
 
-#Write a Python program to print your name.
+# Write a Python program to print your name.
 name="Pankaj Pratap Singh"
 print(name)
 
-#Write a Python program to add two numbers.
+# Write a Python program to add two numbers.
 a=5
 b=6
 sumoftwo=a+b
 print("Sum of a and b is: ", sumoftwo)
 
-#Write a Python program to subtract two numbers.
+# Write a Python program to subtract two numbers.
 a=6
 b=2
 print("Subtract of a and b is: ",a-b)
 
-#Write a Python program to multiply two numbers.
+# Write a Python program to multiply two numbers.
 a=4
 b=3
 print("multiplication of two numbers: ", a*b)
 
-#Write a Python program to divide two numbers and display the result.
+# Write a Python program to divide two numbers and display the result.
 a=6
 b=4
 print("Devidision of two numbers: ",a/b)
 
-#Write a Python program that asks for your age and prints it.
+# Write a Python program that asks for your age and prints it.
 a=int(input("What is your age? "))
 print("Your Age is: ", a)
 
-#Write a Python program to demonstrate the type() function on various variables.
+# Write a Python program to demonstrate the type() function on various variables.
 a=4
 b="Test"
 c=1.4
