3c4ef9e342101f3440defab16ace8e0949fa1cce
Pankaj Pratap SIngh <pankajsys.engg@gmail.com>
2025-05-15 21:20:39 +0400

    commit

---
+++ day03_Mastering_Data_Types/data_type.py
@@ -0,0 +1,37 @@
+print("#Write a Python program to demonstrate data types.")
+a=12
+b="Test"
+print(type(a))
+print(type(b))
+
+print()
+
+print("#Write a Python program to convert an integer to a string and print its type.")
+c=123
+d=str(c)
+print(type(d))
+
+print()
+print("#Write a Python program that checks the data type of a variable using type().")
+e=1.0
+print(type(e))
+
+print()
+
+print("#Write a Python program that performs basic arithmetic operations on integers and floats.")
+print(1+2)
+print(23.0 + 3)
+print()
+
+print("#Write a Python program that demonstrates the use of Boolean values.")
+print(1 == 2)
+
+print()
+
+print("#Write a Python program to add two floating-point numbers.")
+print()
+print("#Write a Python program to display a complex number and its type.")
+print()
+print("#Write a Python program to demonstrate string manipulation (concatenate and length).")
+
+print()
