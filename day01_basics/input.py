138203b2b4b5375215e97fb9f601a14c91bdcdd8
Pankaj Pratap SIngh <pankajsys.engg@gmail.com>
2025-05-27 05:56:36 +0400

    commit

--- day01_basics/input.py
+++
@@ -1,3 +0,0 @@
-# Write a Python program that asks for your age and prints it.
-a=int(input("What is your age? "))
-print("Your Age is: ", a)
--- day01_basics/type.py
+++
@@ -1,9 +0,0 @@
-# Write a Python program to demonstrate the type() function on various variables.
-a=4
-b="Test"
-c=1.4
-d={"name":"Pankaj","Age":34}
-type(a)
-type(b)
-type(c)
-type(d)
